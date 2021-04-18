83d9bc52fd69c6b0db190efb4630160f187e8bfb
凌柏超 <1585638808@qq.com>
2021-04-18 11:31:21 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,6 @@ module.exports = {
   publicPath: '/charts',
   css: {
     loaderOptions: {
-      postcss: {
-        implementation: require('postcss'),
-        plugins: [require('tailwindcss'), require('autoprefixer')],
-      },
       sass: {
         implementation: require('sass'), // This line must in sass option
       },
