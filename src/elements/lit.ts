f16559716736d2ccabc88d75c0da1145c5f96d73
lbc19920615 <1585638808@qq.com>
2021-04-25 10:14:33 +0800

    remove useless

--- src/elements/lit.ts
+++ src/elements/lit.ts
@@ -1,18 +1,7 @@
-import {html, css, LitElement} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {html, LitElement} from 'lit';
+import {customElement} from 'lit/decorators.js';
 
 import './dialog/dialog.ts'
-@customElement('simple-greeting')
-export class SimpleGreeting extends LitElement {
-  static styles = css`p { color: blue }`;
-
-  @property()
-  name = 'Somebody';
-
-  render() {
-    return html`<p>Hello, ${this.name}!</p>`;
-  }
-}
 
 @customElement('fui-section')
 export class FuiSectionElement extends LitElement {
